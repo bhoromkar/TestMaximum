d495f99d1fcf8601f90d484262e8074997dd5015
bhoromkar <118914116+bhoromkar@users.noreply.github.com>
2023-04-18 19:43:25 +0530

    Uc2_added

--- TestMaximum.java
+++ TestMaximum.java
@@ -3,9 +3,9 @@ import java.util.*;
 public class TestMaximum {
 
     public static void main(String[] args) {
-        Integer obj1 = (97);
-        Integer obj2 = (98);
-        Integer obj3 = (35);
+        Float obj1 = (20.37F);
+        Float obj2 = (98.15F);
+        Float obj3 = (35.21f);
         if(obj1.compareTo(obj2)>0 && (obj1.compareTo(obj3)>0)){
             System.out.println(obj1);
         }else if(obj2.compareTo(obj3)>0){
